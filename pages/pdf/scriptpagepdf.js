54cee32e9dd7ad815f0d2bcc8327d4210b3e8b4c
Caio Vinicius <137227050+cvinicius369@users.noreply.github.com>
2023-09-17 23:53:48 -0300

    pages/pdf/scriptpagepdf.js

--- pages/pdf/scriptpagepdf.js
+++ pages/pdf/scriptpagepdf.js
@@ -1,5 +1,5 @@
 class Livros{
     ACS_ASC(){
-        window.open('/library/A Sombra do Corvo/A Canção do Sangue.pdf');
+        window.open('../../library/A Sombra do Corvo/A Canção do Sangue.pdf');
     }
 }
