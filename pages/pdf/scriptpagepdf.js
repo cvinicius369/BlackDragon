0c52257495f5e290b1fd61ff8e7e357e44d33ef6
Caio Vinicius <137227050+cvinicius369@users.noreply.github.com>
2023-09-18 23:10:39 -0300

    index.html script.js style.css pages/pdf/pagepdf.html pages/pdf/scriptpagepdf.js pages/pdf/stylepagepdf.css

--- pages/pdf/scriptpagepdf.js
+++ pages/pdf/scriptpagepdf.js
@@ -1,5 +1,11 @@
-class Livros{
-    ACS_ASC(){
-        window.open('../../library/A Sombra do Corvo/A Canção do Sangue.pdf');
+class menubartopdf{
+    wpp(){
+        window.open('wa.me/62993882350');
+    }
+    homepage(){
+        window.open('https://cvinicius369.github.io/BlackDragon/#');
+    }
+    help(){
+        window.open('');
     }
 }
---
+++ script.js
@@ -0,0 +1,11 @@
+class menubar{
+    wpp(){
+        window.open('wa.me/62993882350');
+    }
+    books(){
+        window.open('');
+    }
+    help(){
+        window.open('');
+    }
+}
