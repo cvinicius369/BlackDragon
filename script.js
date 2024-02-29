bfd80fc6ec4dc82d9285c2b8f50ed3fd6f041547
Caio Vinicius <137227050+cvinicius369@users.noreply.github.com>
2024-02-29 14:31:34 +0000

    Changes

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 class menubar{
-    wpp(){
-        window.open('https://wa.me/62993882350');
-    }
-    det(){
-        window.open('./pages/help/help.html');
-    }
+  wpp(){
+      window.open('https://wa.me/62993882350');
+  }
+  det(){
+      window.open('./pages/help/help.html');
+  }
 }
 
 var menuItems = document.querySelectorAll('.dp-menu-books > ul > li'); // Seleciona todos os elementos do menu
 
-menuItems.forEach(function(menuItem) {  
-                                   // Adiciona um evento de clique a cada elemento do menu
-  menuItem.addEventListener('click', function(event) {
-    event.preventDefault();                                            // Previne o comportamento padrão do link
-    menuItems.forEach(function(item) {                                 // Esconde todos os submenus
-      if (item !== menuItem) {
-        var submenu = item.querySelector('ul');
-        if (submenu) {
-          submenu.style.display = 'none';
-        }
+menuItems.forEach(function(menuItem) {
+                                 // Adiciona um evento de clique a cada elemento do menu
+menuItem.addEventListener('click', function(event) {
+  event.preventDefault();                                            // Previne o comportamento padrão do link
+  menuItems.forEach(function(item) {                                 // Esconde todos os submenus
+    if (item !== menuItem) {
+      var submenu = item.querySelector('ul');
+      if (submenu) {
+        submenu.style.display = 'none';
       }
-    });
-
-    var submenu = this.querySelector('ul');                            // Mostra o submenu do item clicado
-    if (submenu) {
-      submenu.style.display = submenu.style.display === 'none' ? 'block' : 'none';
     }
   });
+
+  var submenu = this.querySelector('ul');                            // Mostra o submenu do item clicado
+  if (submenu) {
+    submenu.style.display = submenu.style.display === 'none' ? 'block' : 'none';
+  }
+});
 });
 
 const menubarra = new menubar();
