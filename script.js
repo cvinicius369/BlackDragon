6cede04bbb137383a598f364d5e0188f9e7dee3d
Caio Vinicius <137227050+cvinicius369@users.noreply.github.com>
2024-02-29 13:40:53 +0000

    Changes

--- script.js
+++ script.js
@@ -6,4 +6,27 @@ class menubar{
         window.open('./pages/help/help.html');
     }
 }
+
+var menuItems = document.querySelectorAll('.dp-menu-books > ul > li'); // Seleciona todos os elementos do menu
+
+menuItems.forEach(function(menuItem) {  
+                                   // Adiciona um evento de clique a cada elemento do menu
+  menuItem.addEventListener('click', function(event) {
+    event.preventDefault();                                            // Previne o comportamento padrão do link
+    menuItems.forEach(function(item) {                                 // Esconde todos os submenus
+      if (item !== menuItem) {
+        var submenu = item.querySelector('ul');
+        if (submenu) {
+          submenu.style.display = 'none';
+        }
+      }
+    });
+
+    var submenu = this.querySelector('ul');                            // Mostra o submenu do item clicado
+    if (submenu) {
+      submenu.style.display = submenu.style.display === 'none' ? 'block' : 'none';
+    }
+  });
+});
+
 const menubarra = new menubar();
